a63a040120934e2517088e0e6d9a20934af7f8ae
Joshdb-18 <joshuadami17@gmail.com>
2023-04-17 12:43:00 +0100

    blocked scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const newTask = true;
-    const newTask2 = false;
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
