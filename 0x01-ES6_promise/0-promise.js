a80585241eefb70c6e9ec7c2672bda66211a450a
Joshdb-18 <joshuadami17@gmail.com>
2023-04-19 19:36:05 +0100

    return a promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,4 @@
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    fetch('https://example.com/api')
-      .then(response => response.json())
-      .then(data => resolve(data))
-      .catch(error => reject(error));
+  return new Promise(() => {
   });
 }
-
