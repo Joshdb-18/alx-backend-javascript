5f82ea7f1770039da3a3d1c1718a87fc73968adc
Joshdb-18 <joshuadami17@gmail.com>
2023-06-12 21:56:02 +0100

    Small server using Node's HTTP module :white_check_mark:

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,5 +1,7 @@
 const http = require('http');
 
+const PORT = 1245
+const HOST = 'localhost'
 // Create the HTTP server
 const app = http.createServer((req, res) => {
   // Set the response headers
@@ -13,7 +15,9 @@ const app = http.createServer((req, res) => {
 });
 
 // Set the server to listen on port 1245
-app.listen(1245);
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
 
 // Export the app variable
 module.exports = app;
