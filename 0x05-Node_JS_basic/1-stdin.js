30742c1e9dc9c82026b64a9dac0f6a34a5f7ce44
Joshdb-18 <joshuadami17@gmail.com>
2023-06-12 14:17:31 +0100

    Using Process stdin :white_check_mark:

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,19 +1,13 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
 
-console.log("Welcome to Holberton School, what is your name?");
-
-rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-  rl.close();
-});
-
-rl.on('close', () => {
-  if (!process.stdin.isTTY) {
-    console.log("This important software is now closing");
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
   }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
